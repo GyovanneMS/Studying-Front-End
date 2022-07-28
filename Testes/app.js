6736e600303d1c3c184cc6de1f5abd2f615bf11c
GYOVANNE MARTINS SABARÁ <22102360@dsn127m1242376.cfp127aluno.local>
2022-07-28 09:36:22 -0300

    Add all

---
+++ Testes/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+function juntarNomeSobrenome (){
+
+const nome = document.getElementById('nome').value;
+const sobrenome = document.getElementById('sobrenome').value;
+const resultado = document.getElementById('resultado');
+
+const nomeCompleto = `Seu nome completo é: ${nome} ${sobrenome}`;
+
+resultado.textContent = nomeCompleto
+}
+
+document.getElementById('juntar').addEventListener('click', juntarNomeSobrenome);
